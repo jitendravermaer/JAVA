879201d20f6520d2a691b34c01f8faacc3a49bc4
JITENDRA VERMA <167751340+jitendravermaer@users.noreply.github.com>
2025-01-22 18:31:22 +0530

    Delete File Handling directory

--- File Handling/FileBuffered.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class FileBuffered {
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bf = new BufferedWriter(new FileWriter("./myfile.txt", true));
-        bf.write("Computer");
-        bf.close();
-    }
-}
